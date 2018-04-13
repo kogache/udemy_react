ff4744f16af90587797bf5aa0c8c9941673ed831
h.koga.orso <h_koga@orso.jp>
2018-04-13 19:36:48 +0900

    不要なファイルデリート

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 
-import Greeting from './greeting';
-
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'john',
     };
   }
 
@@ -17,13 +14,6 @@ class App extends Component {
   render() {
     return (
       <div>
-        <input
-          type="text"
-          value={this.state.name}
-          onChange={e => this.handleNameChange(e.target.value)}
-        />
-        <button onClick={() => this.handleNameChange('Bob')}>I am Bob</button>
-        <Greeting name={this.state.name} />
       </div>
     );
   }
