0dad1f3b2ecd825abe81874c92e3463d8cb95546
h.koga.orso <h_koga@orso.jp>
2018-04-16 19:42:24 +0900

    Lecture23 make GeocodeResult component.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 import SearchForm from './SearchForm';
+import GeocodeResult from './GeocodeResult'
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -17,6 +19,11 @@ class App extends Component {
       <div>
         <h1>緯度経度検索</h1>
         <SearchForm onSubmit={place => this.handlePlaceSubmit(place)}/>
+        <GeocodeResult
+          address={this.state.address}
+          lat={this.state.lat}
+          lng={this.state.lat}
+        />
       </div>
     );
   }
