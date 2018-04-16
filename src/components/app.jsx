a35b8fc52fc07b8b982475c473ffeef72ca69a3e
h.koga.orso <h_koga@orso.jp>
2018-04-17 00:01:16 +0900

    Lesson24 緯度経度取得・表示。

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import SearchForm from './SearchForm';
-import GeocodeResult from './GeocodeResult'
+import GeocodeResult from './GeocodeResult';
+
+const GEOCODE_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json';
 
 class App extends Component {
   constructor(props) {
@@ -10,19 +13,54 @@ class App extends Component {
     };
   }
 
+  setErrorMessage(message) {
+    this.setState({
+      address: message,
+      lat: 0,
+      lng: 0,
+    });
+  }
+
   handlePlaceSubmit(place) {
-    console.log(place);
+    axios.get(GEOCODE_ENDPOINT, { params: { address: place } })
+      .then((results) => {
+        console.log(results);
+        const data = results.data;
+        const result = data.results[0];
+        console.log(data.status)
+        switch (data.status) {
+          case 'OK': {
+            const location = result.geometry.location;
+            this.setState({
+              address: result.formatted_address,
+              lat: location.lat,
+              lng: location.lng,
+            });
+            break;
+          }
+          case 'ZERO_RESULTS': {
+            this.setErrorMessage('結果が見つかりませんでした');
+            break;
+          }
+          default: {
+            this.setErrorMessage('エラーが発生しました');
+            break;
+          }
+        }
+      }).catch(() => {
+        this.setErrorMessage('通信に失敗しました');
+      });
   }
 
   render() {
     return (
       <div>
         <h1>緯度経度検索</h1>
-        <SearchForm onSubmit={place => this.handlePlaceSubmit(place)}/>
+        <SearchForm onSubmit={place => this.handlePlaceSubmit(place)} />
         <GeocodeResult
           address={this.state.address}
           lat={this.state.lat}
-          lng={this.state.lat}
+          lng={this.state.lng}
         />
       </div>
     );
